9bde0376aac8b4caa9ed31055ff3b5df79da31b4
David Thomas <davidthomas5412@gmail.com>
2019-03-26 00:55:52 -0400

    grid catalog

--- source/catalog.py
+++ source/catalog.py
@@ -1,5 +1,5 @@
-from astropy.table import join, Table, vstack
-
+import numpy as np
+from astropy.table import Table
 
 class Catalog(object):
 
@@ -29,3 +29,32 @@ class Catalog(object):
     def fromFile(cls, fname):
         table = Table.read(fname, format='csv')
         return cls(table=table)
+
+
+class GridCatalog(Catalog):
+    """
+    Used to make a grid of nxn sources on each listed chip.
+    Assumes boresight is (0,0) and rotation is 0.
+    """
+    def __init__(self, n=5, chips=['R00_S22', 'R04_S20', 'R40_S02', 'R44_S00'], mag=17,
+                 sed='../sky/sed_500.txt'):
+        super(GridCatalog).__init__()
+
+        # lazy imports
+        from lsst.obs.lsst.phosim import PhosimMapper
+        from lsst.afw.cameraGeom import PIXELS, FIELD_ANGLE
+        from lsst.afw.geom import Point2D
+
+        camera = PhosimMapper().camera
+
+        for chip in chips:
+            det = camera[chip]
+            cornersPix = [Point2D(x) for x in det.getBBox().getCorners()]
+            pixels2Field = camera.getTransform(det.makeCameraSys(PIXELS), FIELD_ANGLE)
+            corners = pixels2Field.applyForward(cornersPix)
+            ras = np.rad2deg([c.getX() for c in corners])
+            decs = np.rad2deg([c.getY() for c in corners])
+
+            for ra in np.linspace(min(ras), max(ras), n + 2)[1:-1]:
+                for dec in np.linspace(min(decs), max(decs), n + 2)[1:-1]:
+                    self.addSource(ra, dec, mag, sed)
