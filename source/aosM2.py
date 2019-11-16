e25f542d7b1f0ad2674aaa8e0526170c39de94f4
Bo Xin <bxin@lsst.org>
2019-11-16 00:23:31 +0000

    update senM_35_19_50.txt and bending modes

--- source/aosM2.py
+++ source/aosM2.py
@@ -6,7 +6,7 @@
 import os
 import numpy as np
 import aosCoTransform as ct
-
+from scipy.interpolate import Rbf
 
 class aosM2(object):
 
@@ -19,9 +19,13 @@ class aosM2(object):
         self.bx = aa[:, 0]
         self.by = aa[:, 1]
         self.bz = aa[:, 2:]
-        # !!! we are using M1M3 forces in place of M2 forces
+
+        # M2 forces based on Harris FEA model
         aa = np.loadtxt('%s/../data/M2/M2_1um_force.DAT'%aosSrcDir)
-        self.force = aa[:, :]
+        self.actID = aa[:, 0]
+        self.actx = aa[:, 1]
+        self.acty = aa[:, 2]
+        self.force = aa[:, 3:]
 
         if debugLevel >= 3:
             print('-b2--  %f' % self.bx[33])
@@ -36,12 +40,19 @@ class aosM2(object):
         aa = np.loadtxt('%s/../data/M2/M2_GT_FEA.txt'%aosSrcDir, skiprows=1)
         x, y, _ = ct.ZCRS2M2CRS(self.bx, self.by, self.bz)
         # first two columns are normalized x and y,
-        # should be the same as the x,y converted from the bending modes x and
+        # need to interpolate to the bending modes x and
         # y above
-        self.zdz = aa[:, 2]  # in micron
-        self.hdz = aa[:, 3]  # in micron
-        self.tzdz = aa[:, 4]  # in micron
-        self.trdz = aa[:, 5]  # in micron
+        tx = aa[:, 0]
+        ty = aa[:, 1]
+        # below are in M2 coordinate system, and in micron
+        ip = Rbf(tx, ty, aa[:, 2])
+        self.zdz = ip(x /self.R, y/self.R)
+        ip = Rbf(tx, ty, aa[:, 3])
+        self.hdz = ip(x /self.R, y/self.R)
+        ip = Rbf(tx, ty, aa[:, 4])
+        self.tzdz = ip(x /self.R, y/self.R)
+        ip = Rbf(tx, ty, aa[:, 5])
+        self.trdz = ip(x /self.R, y/self.R)
 
     def getPrintthz(self, zAngle):
         printthz = self.zdz * np.cos(zAngle) \
